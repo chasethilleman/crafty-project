ceb457fdb3b94c7c5684ee26553154b0b4520fc6
Chase Thilleman <chase@appiphony.com>
2025-08-06 12:18:50 -0400

    update

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -5,15 +5,17 @@ export default function ItemsList(props) {
 
   return (
     <>
-      <h2>Your crafty items</h2>
-      <ul className="items-list">{itemsListItems}</ul>
+      <div className="items-list-container">
+        <h2>Your crafty items</h2>
+        <ul className="items-list">{itemsListItems}</ul>
+      </div>
       {props.items.length > 3 && (
         <div className="ready-card">
           <div>
             <h2>Ready to get crafty?</h2>
-            <p>Click the button below to get a craft idea!</p>
+            <p>Click the button to get a craft idea!</p>
           </div>
-          <button onClick={props.getCraft}>Get Craft</button>
+          <button onClick={props.getCraft}>Get Crafty</button>
         </div>
       )}
     </>
